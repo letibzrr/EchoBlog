f103ec176d78dc76fdcc6f1349b0c64f69a78073
Letícia Bezerra <leticiabezerracontato@gmail.com>
2024-09-09 10:05:04 -0300

    RF01 FINALIZADO

--- backEnd/src/controllers/postagemController.js
+++ backEnd/src/controllers/postagemController.js
@@ -5,12 +5,45 @@ import { date, z } from "zod"
 import formatZodError from "../helpers/formatZodError.js"
 
 //VALIDAÇÕES COM ZOD
-const createSchema =  z.object({});
-const IdSchema = z.object({})
-const updateSchema =  z.object({});
+const createSchema =  z.object({
+    titulo: z.string().min(3, {message: "O titulo deve ter pelo menos três caracteres"}).transform((txt)=>txt.toLowerCase()),
+    conteudo: z.string().min(5, {message: " O conteudo deve ter pelo menos cinco caracteres"}),
+    autor: z.string().min(5, {message: " O conteudo deve ter pelo menos cinco caracteres"}),
+});
+const IdSchema = z.object({
+    id: z.string().uuid({message: "Id da postagem é inválido"})
+})
+const updateSchema =  z.object({
+    titulo: z.string().min(3, {message: "O titulo deve ter pelo menos três caracteres"}).transform((txt)=>txt.toLowerCase()),
+    conteudo: z.string().min(5, {message: " O conteudo deve ter pelo menos cinco caracteres"}),
+    autor: z.string().min(5, {message: " O conteudo deve ter pelo menos cinco caracteres"}),
+});
 
 export const  createPostagem = async (request, response) => { //RF01
     //IMPLEMENTAÇÃO DO ZOD
+    const bodyValidation = createSchema.safeParse(request.body);
+    if(!bodyValidation.success){
+        return response.status(400).json({
+            message: "Os dados recebidos do corpo da requisição são inválidos", 
+            detalhes: formatZodError(bodyValidation.error) 
+        });
+    }
+
+    const { titulo, conteudo, autor, imagem }= request.body;
+    const novaPostagem = {
+        titulo,
+        conteudo,
+        autor,
+        dataPublicacao: new Date(),
+        imagem,
+    };
+    try{
+        await Postagem.create(novaPostagem);
+        response.status(201).json({ message: "Postagem Cadastrada" });
+    } catch (error) {
+        console.error(error);
+        response.status(500).json({ message: "Erro ao cadastrar postagem" });
+    }
 }
 export const  getAllPostagem = async (request, response) => { // RF02
 }
